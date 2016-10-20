3cc6e29d9c42a4f9992d4223c0bca4cd22e3c2aa
CHANSE HORTON <chansehorton@yahoo.com>
2016-10-20 14:30:42 -0600

    committing with basic functionality complete

--- js/app.js
+++ js/app.js
@@ -1,23 +1,57 @@
 'use strict';
 
+var buttons = $('.buttons');
+var calcScreen = $('#screen');
+
+$.fn.noDblOps = function(screenStr, str) {
+  if ($.isNumeric(screenStr[(screenStr.length)-1])) {
+    calcScreen.html(screenStr + str);
+  } else {
+    calcScreen.html(screenStr.slice(0,screenStr.length-1) + str);
+  };
+};
+
+$.fn.calculate = function(screenStr) {
+  if (screenStr.includes('/0')) {
+    calcScreen.html('Error');
+  } else if ($.isNumeric(screenStr[(screenStr.length)-1])!==true) {
+    calcScreen.html('Error');
+  } else {
+    calcScreen.html(eval(screenStr));
+  };
+};
 
 
 $( document ).ready(function() {
-  var buttons = $('.buttons');
-  var calcScreen = $('#screen');
 
   buttons.on('click', function(e) {
     if (e.target !== e.currentTarget) {
       var clickedItem = e.target;
+      var calcScrCurrent = calcScreen.html();
+
       switch(clickedItem.innerText) {
+        case 'Error':
+          break;
         case 'C':
           calcScreen.html('');
           break;
         case '=':
-          calcScreen.html(eval(calcScreen.html()));
+          $.fn.calculate(calcScrCurrent);
+          break;
+        case 'x':
+          $.fn.noDblOps(calcScrCurrent, '*');
+          break;
+        case '÷':
+          $.fn.noDblOps(calcScrCurrent, '/');
+          break;
+        case '+':
+          $.fn.noDblOps(calcScrCurrent, '+');
+          break;
+        case '-':
+          $.fn.noDblOps(calcScrCurrent, '-');
           break;
         default:
-          calcScreen.html(calcScreen.html() + clickedItem.innerText);
+          calcScreen.html(calcScrCurrent + clickedItem.innerText);
           break;
       };
     };
