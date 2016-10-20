35e4fb2ec0e3229ed5355b3e551b05d5778a032f
CHANSE HORTON <chansehorton@yahoo.com>
2016-10-20 11:03:50 -0600

    button clicks now log button text to screen

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@
 
 
 $( document ).ready(function() {
-  var buttons = $('#buttons');
-  var calcScreen = $('#screen')
+  var buttons = $('.buttons');
+  var calcScreen = $('#screen');
+
+  buttons.on('click', function(e) {
+    if (e.target !== e.currentTarget) {
+      var clickedItem = e.target;
+
+      calcScreen.html(calcScreen.html() + clickedItem.innerText);
+    };
+  });
 });
